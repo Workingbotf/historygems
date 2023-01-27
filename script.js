bf707376aa0b3b6c159483295b1dd12bc92ac139
Workingbotf <whoahnono@gmail.com>
2023-01-27 16:38:57 -0600

    added script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 if (!localStorage.getItem("auth") == "true") {
     window.location.pathname = "signin.html"
 } else {
-    window.location.pathname = "main.html"
+    if (!localStorage.getItem("isOnWebsite") || localStorage.getItem("isOnWebsite") == "false") {
+        window.location.pathname = "main.html"
+        localStorage.setItem("isOnWebsite", "true")
+    }
+    
 }
+onbeforeunload((() => {localStorage.setItem("isOnWebsite", "false")})); 
