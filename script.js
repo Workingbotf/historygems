db0a7e7a1180ea61051358d6d5b253b222a3c528
Workingbotf <whoahnono@gmail.com>
2023-01-27 16:49:48 -0600

    Changes to be committed: 	modified:   main.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 if (!localStorage.getItem("auth") == "true") {
     window.location.pathname = "signin.html"
 } else {
-    if (!localStorage.getItem("isOnWebsite") || localStorage.getItem("isOnWebsite") == "false") {
-        window.location.pathname = "main.html"
-        localStorage.setItem("isOnWebsite", "true")
-    }
-    
-}
-function onbeforeunload() {
-    localStorage.setItem("isOnWebsite", "false")
-} 
-function onload() {
-    localStorage.setItem("isOnWebsite", "true")
+    window.location.pathname = "main.html"
 }
