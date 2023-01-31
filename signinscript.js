1a4c8fd09cd85fa8a325200dd6d51f6428836663
Workingbotf <whoahnono@gmail.com>
2023-01-30 21:56:39 -0600

    Fixed Most

--- signinscript.js
+++ signinscript.js
@@ -1,5 +1,4 @@
 function submit() {
-    pwd = "gmmm@123"
     if (document.getElementById("user").value == "lokiscifi" && document.getElementById("pwd").value == "nyim@123") {
         localStorage.setItem("auth", "true")
         window.location.pathname = ""
